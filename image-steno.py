61329d8feab4d71b0a732b7ce9e516809098dc2c
aleksa <zabumaphu@gmail.com>
2023-10-15 22:10:56 -0400

    Basic file system added.

--- image-steno.py
+++ image-steno.py
@@ -4,3 +4,38 @@ Date: October 2023
 Description: Image stenography tool made in python.
 """
 
+# Based on https://www.geeksforgeeks.org/image-based-steganography-using-python/
+
+from PIL import Image
+import pyfiglet
+import argparse
+import progressBar
+import time
+
+def banner():
+    """
+    A function to print the image-steno banner to stdout.
+    """
+    
+    result = pyfiglet.figlet_format("Image-Steno")
+    print(result)
+
+
+def createSteno():
+    pBar = progressBar.progressBar("Fetching base image.")
+    pBar.start()
+    time.sleep(2)
+    pBar.setMsg("Encoding Data.")
+    time.sleep(2)
+    pBar.setMsg("Creating destination image.")
+    time.sleep(2)
+    pBar.stop()
+    time.sleep(0.2)
+    return True
+
+def main():
+    banner()
+    createSteno()
+
+if __name__ == "__main__":
+    main()
