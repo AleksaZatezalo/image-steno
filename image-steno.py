918e76579db5d5ce47e3dc56a20dbc070ca8082f
aleksa <zabumaphu@gmail.com>
2023-10-21 21:20:22 -0400

    Project Structure updated.

--- image-steno.py
+++ image-steno.py
@@ -4,4 +4,8 @@ Date: October 2023
 Description: Image stenography tool made in python.
 """
 
-# Based on https://www.geeksforgeeks.org/image-based-steganography-using-python/
+from PIL import Image
+from PIL import ImageTk
+from PIL import Image
+from io import BytesIO
+import os
