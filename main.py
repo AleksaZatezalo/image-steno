a542e23c78a112c204525e98ac05c6aaf6ebaa54
aleksa <zabumaphu@gmail.com>
2023-10-21 20:16:34 -0400

    Hacktober fest first push.

---
+++ main.py
@@ -0,0 +1,33 @@
+from PIL import Image
+import pyfiglet
+import argparse
+import progressBar
+import time
+
+def banner():
+    """
+    A function to print the image-steno banner to stdout.
+    """
+    
+    result = pyfiglet.figlet_format("Image-Steno")
+    print(result)
+
+
+def createSteno():
+    pBar = progressBar.progressBar("Fetching base image.")
+    pBar.start()
+    time.sleep(2)
+    pBar.setMsg("Encoding Data.")
+    time.sleep(2)
+    pBar.setMsg("Creating destination image.")
+    time.sleep(2)
+    pBar.stop()
+    time.sleep(0.2)
+    return True
+
+def main():
+    banner()
+    createSteno()
+
+if __name__ == "__main__":
+    main()
