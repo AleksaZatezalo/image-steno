dc97f6a55bf10491c77f8d5354db734a929b8cab
aleksa <zabumaphu@gmail.com>
2023-10-23 21:22:58 -0400

    Hacktoberfest project complete.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pyfiglet
 import progressBar
 import time
+import optparse
 import image_steno
 
 def banner():
@@ -12,21 +13,34 @@ def banner():
     print(result)
 
 
-def createSteno():
-    pBar = progressBar.progressBar("Fetching base image.")
-    pBar.start()
-    time.sleep(2)
-    pBar.setMsg("Encoding Data.")
-    time.sleep(2)
-    pBar.setMsg("Creating destination image.")
-    time.sleep(2)
-    pBar.stop()
-    time.sleep(0.2)
-    return True
-
-def main():
-    banner()
-    createSteno()
-
 if __name__ == "__main__":
-    main()
+    parser = optparse.OptionParser("usage %python main.py -s <souce file> -t <text to hide> -d <destination image> -e <encrypt>")
+    parser.add_option('-s', dest='source', type='string',help='The source image to steno.')
+    parser.add_option('-t', dest='text', type='string',help='The text to hide')
+    parser.add_option('-d', dest='dest', type='string',help='The destination image within which to hide text.')
+    parser.add_option('-e', dest='encrypt', type='Bool',help='Set to true if hiding data, otherwise omit.')
+    
+    (options, args) = parser.parse_args()
+    if options.interface == None:
+        print(parser.usage)
+        exit(0)
+
+    else:
+        steno = image_steno(options.source, options.text, options.dest)
+
+        if (options.encrypt):
+             pBar = progressBar.progressBar("Fetching base image.")
+             pBar.start()
+             time.sleep(2)
+             pBar.setMsg("Creating destination image.")
+             time.sleep(2)
+             pBar.stop()
+             time.sleep(0.2)
+             steno.hide_text()
+        else:
+            pBar = progressBar.progressBar("Extracting text.")
+            pBar.start()
+            time.sleep(2)
+            steno.extract_text()
+            pBar.stop()
+            time.sleep(0.2)
